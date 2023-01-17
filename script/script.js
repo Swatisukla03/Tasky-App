c9e88876eb012a169df4fafeec963b2bc5dce8cf
Swati <swatisukla16@gmail.com>
2023-01-17 18:31:41 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,25 @@
+//  const person={
+//     fname:"swati",
+//     lname:"sukla",
+//     age:19
+//  };
+
+
+//  let txt="";
+//  for(let x in person){
+//     txt +=person[x]+" ";
+//  }
+
+// delete person.age;
+
+
+
+const person={
+    firstName:"John",
+    lastName:"Deo",
+    id:55566,
+    
+}
+
+
+ document.getElementById("demo").innerHTML=person.fname+ " is "+ person.lname +person.age;
